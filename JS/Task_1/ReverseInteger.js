ab5b7c81b07916673364802ca86b51dd5686d728
astheeknadig <64676602+astheeknadig@users.noreply.github.com>
2022-03-10 16:27:43 +0530

    Update ReverseInteger.js

--- JS/Task_1/ReverseInteger.js
+++ JS/Task_1/ReverseInteger.js
@@ -3,7 +3,9 @@
  * @return {number}
  */
 
-
 var reverse = function(x) {
-    return a = x.toString().split("").reverse().join("");
+    var a = Math.abs(x);
+    var b = Number(a.toString().split("").reverse().join(""));
+    if ((b > Math.pow(2,31)) || (b < Math.pow(-2,31))) {return 0}
+    return (x < 0 ? -1 : 1) * b
 };
